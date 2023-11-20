acc1f5bb1e866fef8e9e03e41c2c8e126b103156
black hunter <blackhunterganzzfct@gmail.com>
2023-11-21 01:22:56 +0700

    change mode development to productions and change AuthController to getting usermenus

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,6 +35,6 @@ class AuthController extends Controller
         return $this->userService->userLogout();
     }
     public function menu() {
-        return response()->json('oke');
+        return $this->userService->getUserMenu();
     }
 }
